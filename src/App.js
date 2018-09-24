02504bf4b424593ed5cd57e3ea9dc3e305f4c6f8
Hannah Lee <leexhannah@gmail.com>
2018-09-24 13:09:13 -0700

    Move API call to App.js file.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React, { Component } from "react";
 import Landing from "./containers/landing";
 import "./App.css";
 
+const apiKey = process.env.REACT_APP_UNSPLASH_API_KEY
+
 class App extends Component {
+  state = {
+    photos: []
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://api.unsplash.com/photos/?client_id=${apiKey}`
+    )
+      .then(resp => { return resp.json() })
+      .then(data => {
+        console.log('data', data);
+        this.setState({ photos: data})
+        console.log('line 20 this.state.photos', this.state.photos);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
   render() {
+    const { photos } = this.state.photos
+
+    console.log('line 29', this.state.photos);
     return (
       <div>
         <Landing />
--- src/containers/landing.js
+++ src/containers/landing.js
@@ -1,26 +1,9 @@
 import React, { Component } from "react";
 
 class Landing extends Component {
-  state = {
-    data: ""
-  };
-
-  componentDidMount() {
-    fetch(
-      "https://api.unsplash.com/photos/?client_id=" +
-        process.env.REACT_APP_UNSPLASH_API_KEY
-    )
-      .then(resp => resp.json())
-      .then(function(data) {
-        console.log("data", data);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
 
   render() {
-    return <div>Hello World</div>;
+    return <div>Landing Component</div>;
   }
 }
 
