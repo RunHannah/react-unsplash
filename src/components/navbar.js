379ffd62cf1d4de0376feb57d191f17c3499d817
Hannah Lee <leexhannah@gmail.com>
2018-10-04 22:52:52 -0700

    Working on rendering hamburger nav using mobile first approach.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = props => {
+  let hamburgerToggle = () => {
+    let linksElement = document.querySelector(".navbarNarrowLinks");
+    if (linksElement.style.display === "block") {
+      linksElement.style.display = "none";
+    } else {
+      linksElement.style.display = "block";
+    }
+  };
+
   return (
     <div className="navbar">
       <Link to="/react-unsplash/" className="unsplash">
@@ -17,9 +26,27 @@ const Navbar = props => {
           />
         </form>
       </li>
-      <Link to="/layout-one">Layout One</Link>
-      <Link to="/layout-two">Layout Two</Link>
-      <Link to="/layout-three">Layout Three</Link>
+      <div className="narbarWide">
+        <Link to="/layout-one">Layout One</Link>
+        <Link to="/layout-two">Layout Two</Link>
+        <Link to="/layout-three">Layout Three</Link>
+      </div>
+      <div className="navbarNarrow">
+        <p style={{ color: "pink" }} onClick={this.hamburgerToggle}>
+          {"\u2630"}
+        </p>
+        <div className="navbarNarrowLinks">
+          <Link to="/layout-one" onClick={this.hamburgerToggle}>
+            Layout One
+          </Link>
+          <Link to="/layout-two" onClick={this.hamburgerToggle}>
+            Layout Two
+          </Link>
+          <Link to="/layout-three" onClick={this.hamburgerToggle}>
+            Layout Three
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
