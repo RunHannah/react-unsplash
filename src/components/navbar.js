78462f081ef066f2f15b883b5e1c092f2c26b103
Hannah Lee <leexhannah@gmail.com>
2018-10-04 23:01:30 -0700

    Change Navbar component from functional to class, and get toggle button to work. Getting close.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = props => {
-  let hamburgerToggle = () => {
+class Navbar extends Component {
+  hamburgerToggle = () => {
     let linksElement = document.querySelector(".navbarNarrowLinks");
     if (linksElement.style.display === "block") {
       linksElement.style.display = "none";
@@ -11,44 +11,46 @@ const Navbar = props => {
     }
   };
 
-  return (
-    <div className="navbar">
-      <Link to="/react-unsplash/" className="unsplash">
-        <i className="fas fa-camera" />
-      </Link>
-      <li className="search">
-        <form className="search" onSubmit={props.getPhotos}>
-          <input
-            className="search-input"
-            type="text"
-            name="searchInput"
-            placeholder="Search free photos"
-          />
-        </form>
-      </li>
-      <div className="narbarWide">
-        <Link to="/layout-one">Layout One</Link>
-        <Link to="/layout-two">Layout Two</Link>
-        <Link to="/layout-three">Layout Three</Link>
-      </div>
-      <div className="navbarNarrow">
-        <p style={{ color: "pink" }} onClick={this.hamburgerToggle}>
-          {"\u2630"}
-        </p>
-        <div className="navbarNarrowLinks">
-          <Link to="/layout-one" onClick={this.hamburgerToggle}>
-            Layout One
-          </Link>
-          <Link to="/layout-two" onClick={this.hamburgerToggle}>
-            Layout Two
-          </Link>
-          <Link to="/layout-three" onClick={this.hamburgerToggle}>
-            Layout Three
-          </Link>
+  render() {
+    return (
+      <div className="navbar">
+        <Link to="/react-unsplash/" className="unsplash">
+          <i className="fas fa-camera" />
+        </Link>
+        <li className="search">
+          <form className="search" onSubmit={this.props.getPhotos}>
+            <input
+              className="search-input"
+              type="text"
+              name="searchInput"
+              placeholder="Search free photos"
+            />
+          </form>
+        </li>
+        <div className="narbarWide">
+          <Link to="/layout-one">Layout One</Link>
+          <Link to="/layout-two">Layout Two</Link>
+          <Link to="/layout-three">Layout Three</Link>
+        </div>
+        <div className="navbarNarrow">
+          <button style={{ color: "pink" }} onClick={this.hamburgerToggle}>
+            {"\u2630"}
+          </button>
+          <div className="navbarNarrowLinks">
+            <Link to="/layout-one" onClick={this.hamburgerToggle}>
+              Layout One
+            </Link>
+            <Link to="/layout-two" onClick={this.hamburgerToggle}>
+              Layout Two
+            </Link>
+            <Link to="/layout-three" onClick={this.hamburgerToggle}>
+              Layout Three
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Navbar;
