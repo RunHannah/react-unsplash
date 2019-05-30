42041ae5be19db575c0536dc7ac604fb7c035a11
Hannah Lee <leexhannah@gmail.com>
2019-05-29 19:59:55 -0700

    Refactor menu search component.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,75 +1,41 @@
 import React, { Component } from 'react';
 
 class Menu extends Component {
+  state = {
+    menuSearch: [
+      'Wallpapers',
+      'Textures & Patterns',
+      'Nature',
+      'Current Events',
+      'Architecture',
+      'Business & Work',
+      'Animals',
+      'Travel',
+      'Fashion',
+      'Food & Drink',
+      'Spirituality',
+      'Experimental',
+      'People',
+      'Health',
+      'Arts & Culture'
+    ]
+  };
+
   handleOnClick = search => {
     this.props.photoSearch(search);
   };
 
   render() {
+    const { menuSearch } = this.state;
+
     return (
       <div className="menu">
         <ul>
-          <li>
-            <a onClick={() => this.handleOnClick('Wallpapers')}>Wallpapers</a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Textures & Patterns')}>
-              Textures & Patterns
-            </a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Nature')}>Nature</a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Current Events')}>
-              Current Events
-            </a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Architecture')}>
-              Architecture
-            </a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Business & Work')}>
-              Business & Work
-            </a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Animals')}>Animals</a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Travel')}>Travel</a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Fashion')}>Fashion</a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Food & Drink')}>
-              Food & Drink
-            </a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Spirituality')}>
-              Spirituality
-            </a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Experimental')}>
-              Experimental
-            </a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('People')}>People</a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Health')}>Health</a>
-          </li>
-          <li>
-            <a onClick={() => this.handleOnClick('Arts & Culture')}>
-              Arts & Culture
-            </a>
-          </li>
+          {menuSearch.map(search => (
+            <li key={search}>
+              <a onClick={() => this.handleOnClick(search)}>{search}</a>
+            </li>
+          ))}
         </ul>
       </div>
     );
