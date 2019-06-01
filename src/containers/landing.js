fe1c232a941aca019f7a732d7817fce3eb7ccd84
Hannah Lee <leexhannah@gmail.com>
2019-06-01 14:33:42 -0700

    Render landscape photos on landing component.

--- src/containers/landing.js
+++ src/containers/landing.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import Grid from './grid';
 
 class Landing extends Component {
   state = {
-    url: '',
+    photosWide: [],
     isLoading: true
   };
 
   componentDidMount() {
     // find only landscape photos with width > 4000
-    const photos = [];
+    let photos = [];
 
     this.props.photos.map(photo => {
       if (photo.width > '4000') {
@@ -18,23 +17,24 @@ class Landing extends Component {
       return photos;
     });
 
-    const photosLength = photos.length;
-    const number = Math.floor(Math.random() * photosLength);
-    const url = photos[number].urls.regular;
-
-    this.setState({ url, isLoading: false });
+    this.setState({ photosWide: photos, isLoading: false });
   }
 
   render() {
+    const { photosWide } = this.state;
     return (
       <div className="landing">
         {this.state.isLoading ? (
           <p>Loading Images...</p>
         ) : (
           <div>
-            <p className="landing-text" />
-            <img src={this.state.url} alt="unsplash" />
-            <Grid photos={this.props.photos} />
+            {photosWide.map(photo => (
+              <img
+                key={photo.id}
+                src={photo.urls.regular}
+                alt="random-unsplash"
+              />
+            ))}
           </div>
         )}
       </div>
