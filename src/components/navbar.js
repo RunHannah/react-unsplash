3d5c2ddb7ba1df7d2cea78e502ce4cf0d0f021cb
Hannah Lee <leexhannah@gmail.com>
2019-06-01 17:20:28 -0700

    Refactor navbar and media styles.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,47 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
-  hamburgerToggle = () => {
-    let linksElement = document.querySelector('.navbarNarrowLinks');
-    if (linksElement.style.display === 'block') {
-      linksElement.style.display = 'none';
-    } else {
-      linksElement.style.display = 'block';
-    }
-  };
-
   render() {
     return (
-      <div className="navbar">
-        <Link to="/react-unsplash/" className="camera">
-          <i className="fas fa-camera" />
-        </Link>
-        <li className="search">
-          <form className="search" onSubmit={this.props.getPhotos}>
-            <input
-              className="search-input"
-              type="text"
-              name="searchInput"
-              placeholder="Search free photos"
-            />
-          </form>
-        </li>
-        <div className="navbarWide">
+      <div className="mainNav">
+        <div className="navbar">
+          <Link to="/react-unsplash/" className="camera">
+            <i className="fas fa-camera" />
+          </Link>
+          <li className="search">
+            <form className="search" onSubmit={this.props.getPhotos}>
+              <input
+                className="search-input"
+                type="text"
+                name="searchInput"
+                placeholder="Search free photos"
+              />
+            </form>
+          </li>
+        </div>
+        <div className="navLayouts">
           <Link to="/grid">Grid</Link>
           <Link to="/flexbox">Flexbox</Link>
-        </div>
-        <div className="navbarNarrow">
-          <button style={{ color: 'pink' }} onClick={this.hamburgerToggle}>
-            {'\u2630'}
-          </button>
-          <div className="navbarNarrowLinks">
-            <Link to="/grid" onClick={this.hamburgerToggle}>
-              Grid
-            </Link>
-            <Link to="/flexbox" onClick={this.hamburgerToggle}>
-              Flexbox
-            </Link>
-          </div>
         </div>
       </div>
     );
